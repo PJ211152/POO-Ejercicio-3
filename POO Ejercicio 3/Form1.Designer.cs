b0651e8730fe227d7669b4b1b2124ea75fdb4be7
Xavier Perez <perezxavier918@gmail.com>
2021-08-31 20:49:31 -0600

    Interfaz creada

--- POO Ejercicio 3/Form1.Designer.cs
+++ POO Ejercicio 3/Form1.Designer.cs
@@ -28,20 +28,163 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txt_b = new System.Windows.Forms.TextBox();
+            this.txt_c = new System.Windows.Forms.TextBox();
+            this.txt_a = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_x1 = new System.Windows.Forms.TextBox();
+            this.txt_x2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_Calcular = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txt_b
+            // 
+            this.txt_b.Location = new System.Drawing.Point(45, 75);
+            this.txt_b.Name = "txt_b";
+            this.txt_b.Size = new System.Drawing.Size(49, 20);
+            this.txt_b.TabIndex = 0;
+            // 
+            // txt_c
+            // 
+            this.txt_c.Location = new System.Drawing.Point(45, 101);
+            this.txt_c.Name = "txt_c";
+            this.txt_c.Size = new System.Drawing.Size(49, 20);
+            this.txt_c.TabIndex = 1;
+            // 
+            // txt_a
+            // 
+            this.txt_a.Location = new System.Drawing.Point(45, 49);
+            this.txt_a.Name = "txt_a";
+            this.txt_a.Size = new System.Drawing.Size(49, 20);
+            this.txt_a.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(18, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "c";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(374, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Calcule formulas cuadraticas de la forma ax^2+bx+c con la formula cuadratica";
+            // 
+            // txt_x1
+            // 
+            this.txt_x1.Location = new System.Drawing.Point(175, 75);
+            this.txt_x1.Name = "txt_x1";
+            this.txt_x1.Size = new System.Drawing.Size(49, 20);
+            this.txt_x1.TabIndex = 7;
+            // 
+            // txt_x2
+            // 
+            this.txt_x2.Location = new System.Drawing.Point(175, 102);
+            this.txt_x2.Name = "txt_x2";
+            this.txt_x2.Size = new System.Drawing.Size(49, 20);
+            this.txt_x2.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(135, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 24);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "X1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(135, 102);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 24);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "X2";
+            // 
+            // btn_Calcular
+            // 
+            this.btn_Calcular.Location = new System.Drawing.Point(159, 197);
+            this.btn_Calcular.Name = "btn_Calcular";
+            this.btn_Calcular.Size = new System.Drawing.Size(75, 23);
+            this.btn_Calcular.TabIndex = 11;
+            this.btn_Calcular.Text = "Calcular";
+            this.btn_Calcular.UseVisualStyleBackColor = true;
+            this.btn_Calcular.Click += new System.EventHandler(this.btn_Calcular_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(574, 331);
+            this.ClientSize = new System.Drawing.Size(394, 245);
+            this.Controls.Add(this.btn_Calcular);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_x2);
+            this.Controls.Add(this.txt_x1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_a);
+            this.Controls.Add(this.txt_c);
+            this.Controls.Add(this.txt_b);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Encontra raices con la formula cuadratica";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txt_b;
+        private System.Windows.Forms.TextBox txt_c;
+        private System.Windows.Forms.TextBox txt_a;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_x1;
+        private System.Windows.Forms.TextBox txt_x2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btn_Calcular;
     }
 }
 
--- POO Ejercicio 3/Form1.cs
+++ POO Ejercicio 3/Form1.cs
@@ -16,5 +16,30 @@ namespace POO_Ejercicio_3
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txt_x1.Enabled = false;
+            txt_x2.Enabled = false;
+        }
+
+        private void btn_Calcular_Click(object sender, EventArgs e)
+        {
+            double a=0, b=0, c=0, x1=0, x2=0;
+
+            a = Convert.ToDouble(txt_a.Text);
+            b = Convert.ToDouble(txt_b.Text);
+            c = Convert.ToDouble(txt_c.Text);
+
+            try
+            {
+
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor ingresar caracteres validos");
+            }
+
+        }
     }
 }
